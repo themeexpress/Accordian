2e81ddb878e2ae08000407c85e7460a3f5e181a4
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2022-02-28 08:57:34 +0900

    Initial commit

---
+++ src/Question.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { FaMinus, FaPlus } from 'react-icons/fa';
+const Question = ({title, info}) => {
+  const [showInfo, setShowInfo] = useState(false);
+  return <article className='question'>
+    <header>
+      <h4>{title}</h4>
+      <button className='btn' onClick={()=> setShowInfo(!showInfo)}>
+        {showInfo?<FaMinus/> : <FaPlus/>}
+      </button>
+    </header>
+    {showInfo && <p>{info}</p>}
+    </article>;
+};
+
+export default Question;
